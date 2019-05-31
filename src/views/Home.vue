256c2345fcce85f0196807819505162ed22d8a11
boom <linjieyang77@gmail.com>
2019-05-31 16:31:18 +0800

    获取地理位置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,50 @@
 <template>
-  <div class="home">
-    <el-button @click="increment">increment</el-button>
-    <h1>chat app</h1>
-  </div>
+    <div class="home">
+        <el-form>
+            <el-form-item>
+                <el-input v-model="message">
+                    <el-button slot="append" @click="send">send</el-button>
+                </el-input>
+            </el-form-item>
+            <el-button @click="getLoc">获取位置</el-button>
+        </el-form>
+    </div>
 </template>
 
 <script>
 
-    import io from 'socket.io-client';
-    const socket = io('http://localhost:3000');
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000');
 
-    export default {
-        methods: {
-            increment() {
-                socket.emit('increment');
-            },
-            test() {
-                socket.on('countUpdated', (count) => {
-                    console.log('count 已更新', count);
-                });
-            }
-        },
-        mounted() {
-            this.test();
+export default {
+    data() {
+        return {
+            message: ''
         }
+    },
+    methods: {
+        getLoc() {
+            navigator.geolocation.getCurrentPosition(pos => {
+                console.log(pos);
+                let { coords: { latitude, longitude } } = pos
+                socket.emit('sendLocation', { latitude, longitude })
+            })
+        },
+        send() {
+            let { message } = this;
+            socket.emit('sendMessage', message);
+        },
+        increment() {
+            socket.emit('increment');
+        },
+        test() {
+            socket.on('message', (msg) => {
+                console.log(msg);
+            });
+        }
+    },
+    mounted() {
+        this.test();
     }
+}
 </script>
